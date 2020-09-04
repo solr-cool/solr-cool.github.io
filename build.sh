052861a27b17406f8e17c51b1edb2b2181058240
Torsten Bøgh Köster <tbk@thiswayup.de>
2020-09-04 17:09:18 +0200

    reorder build process to gather repo infos

--- build.sh
+++ build.sh
@@ -24,17 +24,17 @@ for plugin in ./_data/packages/*.yaml; do
   name=${file%.yaml}
   descriptor=target/${name}.json
 
-  # check for a manifest section
-  manifest=$(yq -r .manifest $plugin)
-  if [ "$manifest" = "null" ]; then
-    echo "No package manifest given."
+  # -------------------------------------------------------------------
+  # (1) Update package website description
+  # -------------------------------------------------------------------
+  # 
+  package_url=$(yq -r .url $plugin)
+  
+  if [[ ! $package_url == *"https://github.com"* ]]; then
+    echo "Not hosted on Github."
     continue;
   fi
 
-  # -------------------------------------------------------------------
-  # (1) Compile package descriptor
-  # -------------------------------------------------------------------
-  # 
   # prepare descriptor
   jq -n \
     --arg name $name \
@@ -42,23 +42,37 @@ for plugin in ./_data/packages/*.yaml; do
     '{"name":$name, "description": $description}' > ${descriptor}
   
   # read recent versions from Github
-  gh_repo_url=$(yq -r .url $plugin)
-  gh_repo_name=${gh_repo_url#https://github.com/}
-  gh_repo_details=$(curl -s -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/${gh_repo_name}")
-  gh_repo_releases=$(curl -s -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/${gh_repo_name}/releases?per_page=10")
+  gh_repo_name=${package_url#https://github.com/}
+  gh_repo_details=$(curl -u ${GH_USER}:${GH_ACCESS_TOKEN} -s -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/${gh_repo_name}")
+  gh_repo_releases=$(curl -u ${GH_USER}:${GH_ACCESS_TOKEN} -s -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/${gh_repo_name}/releases?per_page=10")
+
+  # extract repo data
+  echo ${gh_repo_details} | jq '{"name": .name, "full_name": .full_name, "description": .description, "updated_at": .updated_at, "stargazers_count": .stargazers_count, "watchers_count": .watchers_count, "license": .license}' > _data/repos/${name}.json
 
   # compile versions
   versions=$(echo ${gh_repo_releases} | jq '[{"version": .[].name , "date": .[].published_at|fromdate|strftime("%Y-%m-%d"), "artifacts": [{ "url": .[].assets[0].browser_download_url }]}]')
 
+  # add versions to plugin descriptor
+  full_descriptor=$(cat ${descriptor} | jq --argjson versions "${versions}" '. += {"versions": $versions}')
+  echo ${full_descriptor} > ${descriptor}
+  cp ${descriptor} _data/versions/${name}.json
+
+  # -------------------------------------------------------------------
+  # (2) Compile package descriptor
+  # -------------------------------------------------------------------
+  #
+  # check for a manifest section
+  manifest=$(yq -r .manifest $plugin)
+  if [ "$manifest" = "null" ]; then
+    echo "No package manifest given."
+    continue;
+  fi
+
   # append manifest to each version
   versions_with_manifest=$(echo ${versions} | jq --argjson manifest "${manifest}" '.[] += {"manifest": $manifest}')
 
-  # add versions to plugin descriptor
-  full_descriptor=$(cat ${descriptor} | jq --argjson versions "${versions_with_manifest}" '. += {"versions": $versions}')
-  echo ${full_descriptor} > ${descriptor}
-
   # -------------------------------------------------------------------
-  # (2) Download & sign JARs
+  # (3) Download & sign JARs
   # -------------------------------------------------------------------
   # find jars to sign
   jars=$(jq -r '.versions[].artifacts[].url' ${descriptor})
@@ -79,13 +93,6 @@ for plugin in ./_data/packages/*.yaml; do
     jq "(.versions[].artifacts[]|select(.url==\"${jar}\")) += {sig: \"${signature}\"}" ${descriptor} | sponge ${descriptor}
   done
   
-  # -------------------------------------------------------------------
-  # (3) Update package website description
-  # -------------------------------------------------------------------
-  cp ${descriptor} _data/versions/${name}.json
-  
-  echo ${gh_repo_details} | jq '{"name": .name, "full_name": .full_name, "description": .description, "updated_at": .updated_at, "stargazers_count": .stargazers_count, "watchers_count": .watchers_count, "license": .license}' > _data/repos/${name}.json
-
   # -------------------------------------------------------------------
   # (4) Generate bats test
   # -------------------------------------------------------------------
